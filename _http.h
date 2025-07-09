20b0d417c9b1c9a5e9df1bd10e7e38c9152a2c63
Shatalov Vladimir <valesh-soft@yandex.ru>
2025-07-09 22:38:45 +0300

    version 0.7.6

---
+++ _http.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "header_file.h"
+
+/*
+ * строка для отправки на Web-страницу
+ * {"ap_ssid":"test","ap_pass":"12345678","ap_ip":"192.168.4.1","threshold":90,"turn_on_delay":3,"max_turn_on_delay":10,"turn_off_delay":20,"max_turn_off_delay":60,"thresh_delay":30, "max_thresh_delay":60,"min_thresh_delay":5}
+ *
+ * строка с Web-страницы для сохранения параметров
+ * {"ap_ssid":"test","ap_pass":"12345678","ap_ip":"192.168.4.1","threshold":90,"turn_on_delay":3,"turn_off_delay":20,"thresh_delay":30}
+ */
